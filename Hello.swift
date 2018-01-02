900a480517706b8b6cbe18c8802d2bd5d77aab16
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 13:24:37 -0800

    Update Hello.swift

--- Hello.swift
+++ Hello.swift
@@ -1,5 +1,16 @@
 import Cocoa
 
+//variables, can change
 var a = 42
 var b = "hello world"
 println(b)
+
+
+//constants, can not change
+let constantName = "value"
+let c = 50
+println(c)
+println(constantName)
+
+//constant float number
+let constD:Float = 3.12
