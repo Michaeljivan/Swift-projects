78e63cf1227c205fca199aa2ae17d58d3d99753c
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 14:00:23 -0800

    Update loops.swift

--- loops.swift
+++ loops.swift
@@ -12,5 +12,5 @@ var array:[Int] = [10, 20, 30]
 var index = 0   //index 0
 //for loop
 for index = 0; index <10; ++index{
-  println("int: [\array[index]]") //print each item from array
+  println("int: array[index]]") //print each item from array
 }
