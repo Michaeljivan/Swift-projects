203116bc6af1f8ddc2901eb648dfaac105f59fe1
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 13:47:23 -0800

    Create loops.swift

---
+++ loops.swift
@@ -0,0 +1,16 @@
+import Cocoa
+/*
+  for loops
+  
+  for init; condition; increment{
+    statement(s)
+  }
+*/
+
+//Integer array
+var array:[Int] = [10, 20, 30]
+var index = 0   //index 0
+//for loop
+for index = 0; index <10; ++index{
+  println("int: [\array[index]]") //print each item from array
+}
