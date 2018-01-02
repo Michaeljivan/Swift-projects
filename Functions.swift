fa85445af83e55d6ee6de0c159e907949a54258a
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 13:58:02 -0800

    Create Functions.swift

---
+++ Functions.swift
@@ -0,0 +1,20 @@
+/* Functions
+
+  func function_name(Parameters) -> returntype
+  {
+    statement(s)
+    return parameters  
+  }
+*/
+//function with parameters
+function student(name: String) -> String{
+  return name
+}
+
+
+//get student name function with no parameters
+ func get_student() -> String
+ {
+    return name
+ }
+ 
